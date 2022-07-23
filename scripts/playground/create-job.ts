dc351918a9480e22d8913d4c583de4d61018c3af
0xultraman <Callegari.lorenzo@gmail.com>
2022-07-23 20:12:51 +0200

    Add some playground scripts

--- scripts/playground/create-job.ts
+++ scripts/playground/create-job.ts
@@ -3,11 +3,12 @@ import { ethers } from "hardhat";
 async function main() {
   const [alice, bob, carol, dave] = await ethers.getSigners()
   const joRe = await ethers.getContractAt('JobRegistry', "0x4aE6B4edD82c75bbA8e29B44aD4829708D5cEc8e")
-  await joRe.connect(bob).createJobFromEmployer(2, `ipfs://ssss`)
+  // await joRe.connect(bob).createJobFromEmployer(1, "ipfs://ssss")
   console.log("Job created");
-  await joRe.connect(carol).confirmJob(1)
+  // await joRe.connect(carol).confirmJob(4)
+  // await joRe.connect(carol).rejectJob(0)
   console.log("Job confirmed");
-  await joRe.connect(bob).finishJob(1)
+  await joRe.connect(bob).finishJob(4)
   console.log("Job finished");
 }
 
