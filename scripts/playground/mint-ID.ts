9845d784bad3a75d3487095f998f470681997ac9
Romain <romain@talentlayer.org>
2022-09-17 11:11:14 +0200

    upgrade mint function

--- scripts/playground/mint-ID.ts
+++ scripts/playground/mint-ID.ts
@@ -2,11 +2,23 @@ import { ethers } from "hardhat";
 
 async function main() {
   const [alice, bob, carol, dave] = await ethers.getSigners()
-  const tlID = await ethers.getContractAt('TalentLayerID', "0x25DFc905884be839eCF2dE0e760C28BA6B8070e6")
-  tlID.connect(bob).mintWithPoh("bob.lens")
-  console.log("Bob.lens registered");
-  tlID.connect(carol).mintWithPoh("carol.lens")
+  console.log({alice: alice.address, bob: bob.address, carol: carol.address})
+  const tlID = await ethers.getContractAt('TalentLayerID', "0x05D8A2E01EB06c284ECBae607A2d0c2BE946Bf49")
+
+  await tlID.connect(alice).mint("alice.lens")
+  console.log("alice.lens registered");
+
+  await tlID.connect(bob).mintWithPoh("bob.lens")
+  console.log("Bob.lens registered")
+
+  await tlID.connect(carol).mintWithPoh("carol.lens")
   console.log("carol.lens registered");
+
+  const supply = await tlID.handles(1);
+  console.log(supply);
+
+  const aliceId = await tlID.walletOfOwner(alice.address)
+  console.log(aliceId)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
