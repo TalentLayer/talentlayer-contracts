9ab2cb05625221c77bb5385aefbd445e6c606aea
Emil Madrell <empaemanuel@tutanota.com>
2022-09-27 13:00:48 +0200

    WIP reworking escrow

--- contracts/tests/SimpleERC20.sol
+++ contracts/tests/SimpleERC20.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleERC20 is ERC20 {
+    address public owner;
+
     constructor() ERC20("SimpleERC20", "SERC20") {
         _mint(msg.sender, 10000);
+        owner = msg.sender;
     }
 
     function decimals() public view virtual override returns (uint8) {
