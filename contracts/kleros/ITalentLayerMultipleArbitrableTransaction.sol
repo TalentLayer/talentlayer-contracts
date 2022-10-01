c23199ea2019b0128edefa80a28cd2c9d444ac84
Emil Madrell <empaemanuel@tutanota.com>
2022-10-01 13:04:39 +0200

    Added transaction check and updated interfaces

--- contracts/kleros/ITalentLayerMultipleArbitrableTransaction.sol
+++ contracts/kleros/ITalentLayerMultipleArbitrableTransaction.sol
@@ -3,16 +3,30 @@ pragma solidity ^0.8.9;
 
 interface ITalentLayerMultipleArbitrableTransaction{
 	struct Transaction {
-	    address sender;
-	    address receiver;
-	    address token;
-	    uint256 amount;
-	}
+        address sender; //pays recipient using the escrow
+        address receiver; //intended recipient of the escrow
+        address token; //token of the escrow
+        uint256 amount; //amount locked into escrow
+        uint256 jobId; //the jobId related to the transaction
+    }
 
 	function createTokenTransaction(
-	    uint256 _jobId,
-	    uint256 _proposalId
-	) external;
+        uint256 _timeoutPayment,
+        string memory _metaEvidence,
+        address _adminWallet,
+        uint256 _adminFeeAmount,
+        uint256 _jobId,
+        uint256 _proposalId
+    ) external;
+
+    function createETHTransaction(
+        uint256 _timeoutPayment,
+        string memory _metaEvidence,
+        address _adminWallet,
+        uint256 _adminFeeAmount,
+        uint256 _jobId,
+        uint256 _proposalId
+    ) external payable;
 
 
 	function release(
