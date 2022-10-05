a83b57b08fb90b25edf90da7f1438b4e8e367e05
Emil Madrell <empaemanuel@tutanota.com>
2022-10-05 13:39:13 +0200

    fixed indentation issue

--- contracts/kleros/ITalentLayerMultipleArbitrableTransaction.sol
+++ contracts/kleros/ITalentLayerMultipleArbitrableTransaction.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 interface ITalentLayerMultipleArbitrableTransaction{
-	struct Transaction {
+    struct Transaction {
         address sender; //pays recipient using the escrow
         address receiver; //intended recipient of the escrow
         address token; //token of the escrow
@@ -10,7 +10,7 @@ interface ITalentLayerMultipleArbitrableTransaction{
         uint256 jobId; //the jobId related to the transaction
     }
 
-	function createTokenTransaction(
+    function createTokenTransaction(
         uint256 _timeoutPayment,
         string memory _metaEvidence,
         address _adminWallet,
@@ -29,13 +29,13 @@ interface ITalentLayerMultipleArbitrableTransaction{
     ) external payable;
 
 
-	function release(
-	    uint256 _transactionId,
-	    uint256 _amount
-	) external;
+    function release(
+        uint256 _transactionId,
+        uint256 _amount
+    ) external;
 
-	function reimburse(
-	    uint256 _transactionId,
-	    uint256 _amount
-	) external;
+    function reimburse(
+        uint256 _transactionId,
+        uint256 _amount
+    ) external;
 }
