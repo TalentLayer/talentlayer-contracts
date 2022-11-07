62101fa390d6740f48fd00a57dca87116d1fc8d0
Akumaw <dev@akumaw.com>
2022-11-07 21:55:07 +0100

    Add config for goerli deploy

--- scripts/playground/7-pay.ts
+++ scripts/playground/7-pay.ts
@@ -1,29 +1,32 @@
-import { ethers } from "hardhat";
-import { get, ConfigProperty } from "../../configManager";
-import { Network } from "../config";
-const hre = require("hardhat");
+import { ethers } from 'hardhat'
+import { get, ConfigProperty } from '../../configManager'
+import { Network } from '../config'
+const hre = require('hardhat')
 
 // Then Alice create a service, and others add proposals
 async function main() {
-  const network = await hre.network.name;
-  console.log(network);
+  const network = await hre.network.name
+  console.log(network)
 
-  const [alice] = await ethers.getSigners();
+  const [alice] = await ethers.getSigners()
   const talentLayerMultipleArbitrableTransaction = await ethers.getContractAt(
-    "TalentLayerMultipleArbitrableTransaction",
-    get(
-      network as Network,
-      ConfigProperty.TalentLayerMultipleArbitrableTransaction
-    )
-  );
+    'TalentLayerMultipleArbitrableTransaction',
+    get(network as Network, ConfigProperty.TalentLayerMultipleArbitrableTransaction),
+  )
 
-  await talentLayerMultipleArbitrableTransaction.connect(alice).release(0, 140);
-  await talentLayerMultipleArbitrableTransaction.connect(alice).release(0, 60);
+  const firstRelease = await talentLayerMultipleArbitrableTransaction.connect(alice).release(0, 140)
+  await firstRelease.wait()
+  console.log('Alice first Release is 140')
+
+  const secondRelease = await talentLayerMultipleArbitrableTransaction.connect(alice).release(0, 60)
+  await secondRelease.wait()
+
+  console.log('Alice second release is 60')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
