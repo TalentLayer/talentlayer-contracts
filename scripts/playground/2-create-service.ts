6fdba619be3f96164ca7404662424c8a38c40e86
Akumaw <dev@akumaw.com>
2022-11-07 22:00:57 +0100

    prettier

--- scripts/playground/2-create-service.ts
+++ scripts/playground/2-create-service.ts
@@ -1,52 +1,49 @@
-import { ethers } from "hardhat";
-import { get, ConfigProperty } from "../../configManager";
-import { Network } from "../config";
-const hre = require("hardhat");
-import postToIPFS from "../ipfs";
+import { ethers } from 'hardhat'
+import { get, ConfigProperty } from '../../configManager'
+import { Network } from '../config'
+const hre = require('hardhat')
+import postToIPFS from '../ipfs'
 
 async function main() {
-  const network = await hre.network.name;
-  console.log(network);
-  console.log("Create service Test start");
+  const network = await hre.network.name
+  console.log(network)
+  console.log('Create service Test start')
 
-  const [alice, bob, carol, dave] = await ethers.getSigners();
+  const [alice, bob, carol, dave] = await ethers.getSigners()
   const serviceRegistry = await ethers.getContractAt(
-    "ServiceRegistry",
-    get(network as Network, ConfigProperty.ServiceRegistry)
-  );
+    'ServiceRegistry',
+    get(network as Network, ConfigProperty.ServiceRegistry),
+  )
 
   const platformIdContrat = await ethers.getContractAt(
-    "TalentLayerPlatformID",
-    get(network as Network, ConfigProperty.TalentLayerPlatformID)
-  );
+    'TalentLayerPlatformID',
+    get(network as Network, ConfigProperty.TalentLayerPlatformID),
+  )
 
-  const daveTalentLayerIdPLatform =
-    await platformIdContrat.getPlatformIdFromAddress(dave.address);
-  console.log("Dave talentLayerIdPLatform", daveTalentLayerIdPLatform);
+  const daveTalentLayerIdPLatform = await platformIdContrat.getPlatformIdFromAddress(dave.address)
+  console.log('Dave talentLayerIdPLatform', daveTalentLayerIdPLatform)
 
   const aliceCreateJobData = await postToIPFS(
     JSON.stringify({
-      title: "Full Stack Developer Job",
-      about: "Looking for Full Stack Developer",
-      keywords: "BlockChain",
-      role: "developer",
-      rateToken: "0x0000000000000000000000000000000000000000",
+      title: 'Full Stack Developer Job',
+      about: 'Looking for Full Stack Developer',
+      keywords: 'BlockChain',
+      role: 'developer',
+      rateToken: '0x0000000000000000000000000000000000000000',
       rateAmount: 1,
-      recipient: "",
-    })
-  );
+      recipient: '',
+    }),
+  )
 
-  console.log("AliceJobDataUri", aliceCreateJobData);
+  console.log('AliceJobDataUri', aliceCreateJobData)
 
-  await serviceRegistry
-    .connect(alice)
-    .createOpenServiceFromBuyer(daveTalentLayerIdPLatform, aliceCreateJobData);
-  console.log("Open Service created");
+  await serviceRegistry.connect(alice).createOpenServiceFromBuyer(daveTalentLayerIdPLatform, aliceCreateJobData)
+  console.log('Open Service created')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
--- scripts/playground/get-tokenuri.ts
+++ scripts/playground/get-tokenuri.ts
@@ -1,15 +1,15 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function main() {
   const [alice, bob, carol, dave] = await ethers.getSigners()
-  const tlID = await ethers.getContractAt('TalentLayerID', "0x25DFc905884be839eCF2dE0e760C28BA6B8070e6")
-  const uri = await tlID.tokenURI(1);
-  console.log(uri);
+  const tlID = await ethers.getContractAt('TalentLayerID', '0x25DFc905884be839eCF2dE0e760C28BA6B8070e6')
+  const uri = await tlID.tokenURI(1)
+  console.log(uri)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
