bc8805cea5976d32004f1c360faa20c79d84ee36
Akumaw <dev@akumaw.com>
2022-11-18 10:26:18 +0100

    Add View getter function

---
+++ scripts/playground/10-getInfos.ts
@@ -0,0 +1,52 @@
+import { ethers } from 'hardhat'
+import { get, ConfigProperty } from '../../configManager'
+import { Network } from '../config'
+const hre = require('hardhat')
+
+async function main() {
+  const network = await hre.network.name
+  console.log('Create service Test start---------------------')
+  console.log(network)
+
+  const [alice, bob, carol, dave] = await ethers.getSigners()
+
+  const serviceRegistry = await ethers.getContractAt(
+    'ServiceRegistry',
+    get(network as Network, ConfigProperty.ServiceRegistry),
+  )
+
+  let serviceId = await serviceRegistry.nextServiceId()
+  console.log('serviceId', serviceId.toString())
+
+  console.log('serviceId', serviceId.toString())
+
+  // Getting all created services
+  console.log('Get All created services---------------------')
+
+  const getAllCreatedServices = await serviceRegistry.getAllServices()
+  console.log('All Services', getAllCreatedServices)
+
+  // Getting all proposals created for a service
+  console.log('Get All created proposals for a service ---------------------')
+
+  const getAllProposalsForService = await serviceRegistry.getAllProposalsForService(serviceId)
+  console.log('All Proposals for a service', getAllProposalsForService)
+
+  // getting all Services created by a specific user
+  console.log('Get All created services by a specific user---------------------')
+
+  const getAllServicesForUser = await serviceRegistry.getAllServicesForUser(1)
+  console.log('Services by user', getAllServicesForUser)
+
+  // getting all proposals created by a specific user
+  console.log('Get All created proposals by a specific user---------------------')
+  const getAllProposalsForUser = await serviceRegistry.getAllProposalsForUser(2)
+  console.log('Services by user', getAllProposalsForUser)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
