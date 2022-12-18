f3f9fec71be3980045f6e9315fb97a4af21d39d8
mattiapomelli <mattiapomelli@gmail.com>
2022-12-18 12:31:02 +0100

    Add tasks to add and remove an arbitrator

---
+++ scripts/tasks/addArbitrator.ts
@@ -0,0 +1,29 @@
+import { task } from 'hardhat/config'
+import { Network } from '../config'
+import { ConfigProperty, get } from '../../configManager'
+
+/**
+ * @notice This task is used to add a new arbitrator to the list of available arbitrators
+ * @param {string} address - The address of the arbitrator
+ * @dev Example of script use: "npx hardhat add-arbitrator --address 0x9e1d8f9Ad75F6fF624D925C313b80C1a98071C89 --is-internal --network goerli"
+ */
+task('add-arbitrator', 'Adds a new available arbitrator')
+  .addParam('address', 'The address of the arbitrator')
+  .addFlag('isInternal', 'Whether the arbitrator is internal or not')
+  .setAction(async (taskArgs, { ethers, network }) => {
+    const { address, isInternal } = taskArgs
+    const [deployer] = await ethers.getSigners()
+
+    console.log('network', network.name)
+
+    const platformIdContract = await ethers.getContractAt(
+      'TalentLayerPlatformID',
+      get(network.name as any as Network, ConfigProperty.TalentLayerPlatformID),
+      deployer,
+    )
+
+    const tx = await platformIdContract.addArbitrator(address, isInternal)
+    await tx.wait()
+
+    console.log(`Added ${isInternal ? 'internal' : 'external'} arbitrator with address: `, address)
+  })
