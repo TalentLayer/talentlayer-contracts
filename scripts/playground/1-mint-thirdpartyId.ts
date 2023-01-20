2cdd4fd46851f219daf012b4c858f6d426483904
Akumaw <dev@akumaw.com>
2023-01-20 22:20:39 +0100

    update mint thirdParty playground script

--- scripts/playground/1-mint-thirdpartyId.ts
+++ scripts/playground/1-mint-thirdpartyId.ts
@@ -11,9 +11,6 @@ async function main() {
 
   const [alice, bob, carol, dave, eve, frank] = await ethers.getSigners()
 
-  const strategiesID = [0, 1]
-  console.log('strategiesID', strategiesID)
-
   const talentLayerIdContract = await ethers.getContractAt(
     'TalentLayerID',
     get(network as Network, ConfigProperty.TalentLayerID),
@@ -49,7 +46,7 @@ async function main() {
   ])
 
   await mockLensHub.addLensProfileManually([
-    alice.address,
+    alice.address, // WARNING array submission start at 1 Alice won't be registered
     bob.address,
     carol.address,
     dave.address,
@@ -57,31 +54,40 @@ async function main() {
     frank.address,
   ])
 
+  // We check if bob is Registerd on ProofOfHumanity and Lens
+  const bobIsRegisteredOnProofOfHumanity = await proofOfHumanityID.isRegistered(bob.address)
+  console.log('Bob Is Registered On ProofOfHumanity', bobIsRegisteredOnProofOfHumanity)
+
+  const bobIsRegisteredOnLens = await lensID.isRegistered(bob.address)
+  console.log('Bob Is Registered On Lens', bobIsRegisteredOnLens)
+
+  // we check if carol is Registerd on ProofOfHumanity
+  const carolIsRegisteredOnProofOfHumanity = await proofOfHumanityID.isRegistered(carol.address)
+  console.log('carol Is Registered On ProofOfHumanity', carolIsRegisteredOnProofOfHumanity)
+
+  // we check that eve is well registerd on Lens
+  const eveIsRegisteredOnLens = await lensID.isRegistered(eve.address)
+  console.log('eve Is Registered On Lens', eveIsRegisteredOnLens)
+
   // We get the platform ID of Dave
   const daveTalentLayerIdPLatform = await platformIdContrat.getPlatformIdFromAddress(dave.address)
-  console.log('Dave talentLayerIdPLatform', daveTalentLayerIdPLatform)
 
-  await talentLayerIdContract.connect(frank).mint(daveTalentLayerIdPLatform, 'frank', strategiesID)
+  await talentLayerIdContract.connect(bob).mint(daveTalentLayerIdPLatform, 'Bob', [0, 1])
+  await talentLayerIdContract.connect(carol).mint(daveTalentLayerIdPLatform, 'Carol', [0])
+  await talentLayerIdContract.connect(eve).mint(daveTalentLayerIdPLatform, 'Eve', [1])
 
-  const frankUserId = await talentLayerIdContract.walletOfOwner(frank.address)
-  console.log('frankUserId', frankUserId)
+  // A few checks
+  const bobUserId = await talentLayerIdContract.walletOfOwner(bob.address)
+  console.log('frankUserId', bobUserId)
 
-  const frankPohId = await talentLayerIdContract.getThirdPartyId(frankUserId, 0)
-  console.log('thirdPartyIdIndexZero', frankPohId)
+  const frankPohId = await talentLayerIdContract.getThirdPartyId(bobUserId, 0)
+  console.log('Bob poh Id', frankPohId)
 
-  const frankLensId = await talentLayerIdContract.getThirdPartyId(frankUserId, 1)
-  console.log('thirdPartyIdIndexOne', frankLensId)
-
-  // we check if frank is Registerd on Lens
-  const frankIsRegisteredOnLens = await lensID.isRegistered(frank.address)
-  console.log('frankIsRegisteredOnLens', frankIsRegisteredOnLens)
-
-  // we check if frank is Registerd on ProofOfHumanity
-  const frankIsRegisteredOnProofOfHumanity = await proofOfHumanityID.isRegistered(frank.address)
-  console.log('frankIsRegisteredOnProofOfHumanity', frankIsRegisteredOnProofOfHumanity)
+  const frankLensId = await talentLayerIdContract.getThirdPartyId(bobUserId, 1)
+  console.log('Bob Lens Id', frankLensId)
 
   // frank address
-  console.log('frank address', frank.address)
+  console.log('bob address', bob.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
