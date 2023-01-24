bbdd854726055f1ab6535ff269c3602ea27fddda
Akumaw <dev@akumaw.com>
2023-01-24 14:45:45 +0100

    Fix abi.encodePacked

--- contracts/ThirdParties/POH/ProofOfHumanityID.sol
+++ contracts/ThirdParties/POH/ProofOfHumanityID.sol
@@ -6,24 +6,20 @@ import "../../interfaces/IThirdPartyID.sol";
 import "./IProofOfHumanity.sol";
 
 contract ProofOfHumanityID is Ownable, IThirdPartyID {
-   
     /**
      * @notice Instance of IProofOfHumanity
      */
     IProofOfHumanity private proofOfHumanity;
 
-    
     constructor(address _address) {
         proofOfHumanity = IProofOfHumanity(_address);
     }
-    
 
     function isRegistered(address _userAddress) external view returns (bool, bytes memory) {
-  
         bool userIsRegistered = proofOfHumanity.isRegistered(_userAddress);
 
         if (userIsRegistered) {
-            return (true, abi.encode(_userAddress));
+            return (true, abi.encodePacked(_userAddress));
         } else {
             return (false, bytes(""));
         }
