c871af7a54bb5fae358a43f8e2e94de3f0a31720
mattiapomelli <mattiapomelli@gmail.com>
2023-01-26 09:30:50 +0000

    Fix formatting

--- scripts/tasks/protocol/mintPlatformIdForAddress.ts
+++ scripts/tasks/protocol/mintPlatformIdForAddress.ts
@@ -19,7 +19,7 @@ task('mint-platform-id', 'Mints platform Ids to addresses')
 
     const platformIdContract = await ethers.getContractAt(
       'TalentLayerPlatformID',
-      get(network.name as any as Network, ConfigProperty.TalentLayerPlatformID),
+      get((network.name as any) as Network, ConfigProperty.TalentLayerPlatformID),
       deployer,
     )
 
