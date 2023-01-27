300b3e2bbb93afc305ae670147801aac73c7948c
mattiapomelli <mattiapomelli@gmail.com>
2023-01-27 11:38:57 +0000

    Update solidity version of ERC2771Recipient

--- contracts/libs/ERC2771RecipientUpgradeable.sol
+++ contracts/libs/ERC2771RecipientUpgradeable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.9;
+pragma solidity ^0.8.9;
 
 import {IERC2771Recipient} from "@opengsn/contracts/src/interfaces/IERC2771Recipient.sol";
 import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
