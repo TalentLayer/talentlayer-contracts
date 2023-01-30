c7c8dbecbeba9295d9531dd683d96ef51cd034bc
mattiapomelli <mattiapomelli@gmail.com>
2023-01-30 09:28:54 +0000

    Create mock forwarder for tests

--- test/batch/gaslessTransactions.ts
+++ test/batch/gaslessTransactions.ts
@@ -1,28 +1,29 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { TalentLayerID } from '../../typechain-types'
+import { MockForwarder, TalentLayerID } from '../../typechain-types'
 import { deploy } from '../utils/deploy'
 
 describe('Gasless Transactions', function () {
-  let deployer: SignerWithAddress, alice: SignerWithAddress, talentLayerID: TalentLayerID
+  let deployer: SignerWithAddress, alice: SignerWithAddress, talentLayerID: TalentLayerID, mockForwarder: MockForwarder
 
   before(async function () {
     ;[deployer, alice] = await ethers.getSigners()
     ;[talentLayerID] = await deploy(false)
+
+    const MockForwarder = await ethers.getContractFactory('MockForwarder')
+    mockForwarder = await MockForwarder.deploy()
   })
 
   it('Deployer can add and remove a trusted forwarder for meta-transactions', async function () {
-    const forwarderAddress = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
-
     // Fails if is not the owner
-    const tx = talentLayerID.connect(alice).addTrustedForwarder(forwarderAddress)
+    const tx = talentLayerID.connect(alice).addTrustedForwarder(mockForwarder.address)
     await expect(tx).to.be.revertedWith('Ownable: caller is not the owner')
 
-    await talentLayerID.connect(deployer).addTrustedForwarder(forwarderAddress)
-    expect(await talentLayerID.isTrustedForwarder(forwarderAddress)).to.be.true
+    await talentLayerID.connect(deployer).addTrustedForwarder(mockForwarder.address)
+    expect(await talentLayerID.isTrustedForwarder(mockForwarder.address)).to.be.true
 
-    await talentLayerID.connect(deployer).removeTrustedForwarder(forwarderAddress)
-    expect(await talentLayerID.isTrustedForwarder(forwarderAddress)).to.be.false
+    await talentLayerID.connect(deployer).removeTrustedForwarder(mockForwarder.address)
+    expect(await talentLayerID.isTrustedForwarder(mockForwarder.address)).to.be.false
   })
 })
