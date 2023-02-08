6522dbf33bca048b747d2081b76de810acbcb207
mattiapomelli <mattiapomelli@gmail.com>
2023-02-08 11:31:26 +0000

    Update TalentLayerdID interface

--- contracts/interfaces/ITalentLayerID.sol
+++ contracts/interfaces/ITalentLayerID.sol
@@ -29,6 +29,8 @@ interface ITalentLayerID {
 
     function getOriginatorPlatformIdByAddress(address _address) external view returns (uint256);
 
+    function isDelegator(address _userAddress, address _address) external view returns (bool);
+
     function isOwnerOrDelegator(uint256 _tokenId, address _address) external view returns (bool);
 
     event Mint(address indexed _user, uint256 _tokenId, string _handle);
