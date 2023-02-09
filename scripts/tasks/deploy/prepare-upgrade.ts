952d460d4876365ccc51bf2885f4811840929fd6
Romain <romain@talentlayer.org>
2023-02-09 09:10:13 +0000

    Update task naming

--- scripts/tasks/deploy/prepare-upgrade.ts
+++ scripts/tasks/deploy/prepare-upgrade.ts
@@ -42,7 +42,7 @@ task('prepare-upgrade', 'Prepare an upgrade of a new implementation for one of t
     if (automaticProposal) {
       // @ts-ignore: defender is imported in hardhat.config.ts
       const proposal = await defender.proposeUpgrade(proxyAddress, NewImplementation, {
-        multisig: networkConfig.multisigAddress,
+        multisig: networkConfig.multisigDeployerAddress,
       })
       newImplementationAddress = proposal.metadata.newImplementationAddress
       console.log('Upgrade proposal created at:', proposal.url)
