346e18f2d924c11e3519abe576d3373999f73a2e
akugone <dev@akumaw.com>
2023-02-15 14:51:43 +0000

    add task to update the platform minting status

---
+++ scripts/tasks/protocol/update-whitelist-status.ts
@@ -0,0 +1,25 @@
+import { task } from 'hardhat/config'
+import { DeploymentProperty, getDeploymentProperty } from '../../../.deployment/deploymentManager'
+
+/**
+ * @notice This task allow to update the platform id minting status
+ * @param {uint256} status - The platform id minting status (0 = ON_PAUSE 1 = ONLY_WHITELIST, 2 = PUBLIC)
+ * @dev Example of script use: "npx hardhat update-whitelist-status --status ONLY_WHITELIST --network mumbai"
+ */
+task('update-whitelist-status', 'change the platform id minting status')
+  .addParam('status', 'The platform id minting status')
+  .setAction(async (taskArgs, { ethers, network }) => {
+    const { status } = taskArgs
+    const [deployer] = await ethers.getSigners()
+
+    console.log('network', network.name)
+
+    const platformIdContract = await ethers.getContractAt(
+      'TalentLayerPlatformID',
+      getDeploymentProperty(network.name, DeploymentProperty.TalentLayerPlatformID),
+      deployer,
+    )
+
+    await platformIdContract.updateMintStatus(status)
+    console.log(`the platform id minting status is ${status}`)
+  })
