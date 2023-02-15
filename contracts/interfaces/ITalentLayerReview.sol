886700f92191e31eec9bcc5beb88cc61225874ce
Quentin D.C <quent043@hotmail.com>
2023-02-15 07:29:19 +0000

    Added rating & serviceId in Review Struct

---
+++ contracts/interfaces/ITalentLayerReview.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+/**
+ * @title TalentLayer Review Contract
+ * @author TalentLayer Team
+ */
+contract ITalentLayerReview {
+
+    // Struct Review
+    struct Review {
+        uint256 id;
+        uint256 owner;
+        string dataUri;
+        uint256 platformId;
+        uint256 serviceId;
+        uint256 rating;
+    }
+
+    error ReviewAlreadyMinted();
+
+    function getReview(uint256 _reviewId) public view returns (Review memory);
+
+    function addReview(
+        uint256 _tokenId,
+        uint256 _serviceId,
+        string calldata _reviewUri,
+        uint256 _rating,
+        uint256 _platformId
+    ) external;
+
+}
