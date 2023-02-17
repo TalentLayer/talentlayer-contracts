c1e3efc29f6acf6c9faf3db2c44c63206d10aa29
Romain <romain@talentlayer.org>
2023-02-17 09:03:11 +0000

    remove unused import

--- contracts/TalentLayerEscrow.sol
+++ contracts/TalentLayerEscrow.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
