0a1916c7e0540f2f5d996207644b99910070ae57
akugone <dev@akumaw.com>
2023-02-20 16:41:31 +0000

    fix grammar

--- contracts/TalentLayerService.sol
+++ contracts/TalentLayerService.sol
@@ -359,8 +359,8 @@ contract TalentLayerService is Initializable, ERC2771RecipientUpgradeable, UUPSU
      * @dev Only the contract owner can call this function
      */
     function updateAllowedTokenList(address _tokenAddress, bool _status) public onlyOwner {
-        if(_tokenAddress == address(0) && _status == false ){
-            revert("Owner can't remove Ox address")
+        if (_tokenAddress == address(0) && _status == false) {
+            revert("Owner can't remove Ox address");
         }
         allowedTokenList[_tokenAddress] = _status;
 
