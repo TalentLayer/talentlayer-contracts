ef03c79686ca884ffd1811ccac55a4f60593ba8d
Hutgin <dev@akumaw.com>
2023-02-21 12:45:19 +0000

    Min tx fee allowed token (#146)
    * fix mint return
    
    * Fix unit test
    
    * add minTransactionAmount in config
    
    * fix PR
    
    * Upgrade interface
    
    * minor fix
    
    * Fix PR
    
    * fix naming camelCase
    
    ---------
    
    Co-authored-by: Romain <romain@talentlayer.org>

--- networkConfig.ts
+++ networkConfig.ts
@@ -10,7 +10,13 @@ export enum Network {
 
 export type NetworkConfig = {
   multisigAddressList: { deployer?: `0x${string}`; fee: `0x${string}` }
-  allowedTokenList: { [key: string]: `0x${string}` }
+  allowedTokenList: {
+    [key: string]: {
+      address: `0x${string}`
+      minTransactionAmount: string
+      decimals: number
+    }
+  }
   platformList: { [name: string]: `0x${string}` }
 }
 
@@ -19,7 +25,11 @@ const local: NetworkConfig = {
     fee: '0x3Fba71369E5E2E947AE2320274b1677de7D28120',
   },
   allowedTokenList: {
-    ETH: ethers.constants.AddressZero,
+    ETH: {
+      address: ethers.constants.AddressZero,
+      minTransactionAmount: '0.001',
+      decimals: 18,
+    },
   },
   platformList: {
     hirevibes: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
@@ -27,33 +37,49 @@ const local: NetworkConfig = {
   },
 }
 
-const fuji = {
+const fuji: NetworkConfig = {
   multisigAddressList: {
     fee: '0x_TODO_CREATE_MULTISIG',
   },
   allowedTokenList: {
-    AVAX: ethers.constants.AddressZero,
-    USDC: '0xAF82969ECF299c1f1Bb5e1D12dDAcc9027431160',
+    AVAX: {
+      address: ethers.constants.AddressZero,
+      minTransactionAmount: '0.1',
+      decimals: 18,
+    },
+    USDC: {
+      address: '0xAF82969ECF299c1f1Bb5e1D12dDAcc9027431160',
+      minTransactionAmount: '1',
+      decimals: 6,
+    },
   },
   platformList: {
     hirevibes: '0x96573C632c88996711de69389b501F4D9005Ff4e',
   },
-} as NetworkConfig
+}
 
-const mumbai = {
+const mumbai: NetworkConfig = {
   multisigAddressList: {
     fee: '0x3Fba71369E5E2E947AE2320274b1677de7D28120',
     deployer: '0x99f117069F9ED15476003502AD8D96107A180648',
   },
   allowedTokenList: {
-    MATIC: ethers.constants.AddressZero,
-    USDC: '0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747',
+    MATIC: {
+      address: ethers.constants.AddressZero,
+      minTransactionAmount: '1',
+      decimals: 18,
+    },
+    USDC: {
+      address: '0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747',
+      minTransactionAmount: '1',
+      decimals: 6,
+    },
   },
   platformList: {
     hirevibes: '0x96573C632c88996711de69389b501F4D9005Ff4e',
     workpod: '0x4444F618BA8E99435E721abF3c611D5105A407e9',
   },
-} as NetworkConfig
+}
 
 const avalanche = {} as NetworkConfig
 const polygon = {} as NetworkConfig
--- test/batch/fullWorkflow.ts
+++ test/batch/fullWorkflow.ts
@@ -24,7 +24,7 @@ const bobPlatformId = 2
 
 const now = Math.floor(Date.now() / 1000)
 const proposalExpirationDate = now + 60 * 60 * 24 * 15
-const minTokenWhitelistTranscationFees = 100
+const minTokenWhitelistTransactionAmount = 10
 
 describe('TalentLayer protocol global testing', function () {
   // we define the types of the variables we will use
@@ -104,7 +104,7 @@ describe('TalentLayer protocol global testing', function () {
     for (const tokenAddress of allowedTokenList) {
       await talentLayerService
         .connect(deployer)
-        .updateAllowedTokenList(tokenAddress, true, minTokenWhitelistTranscationFees)
+        .updateAllowedTokenList(tokenAddress, true, minTokenWhitelistTransactionAmount)
     }
 
     // Disable whitelist for reserved handles
@@ -642,7 +642,7 @@ describe('TalentLayer protocol global testing', function () {
       await expect(
         talentLayerService
           .connect(alice)
-          .updateAllowedTokenList(token.address, true, minTokenWhitelistTranscationFees),
+          .updateAllowedTokenList(token.address, true, minTokenWhitelistTransactionAmount),
       ).to.be.revertedWith('Ownable: caller is not the owner')
     })
 
@@ -653,7 +653,7 @@ describe('TalentLayer protocol global testing', function () {
           .updateAllowedTokenList(
             ethers.constants.AddressZero,
             false,
-            minTokenWhitelistTranscationFees,
+            minTokenWhitelistTransactionAmount,
           ),
       ).to.be.revertedWith("Owner can't remove Ox address")
     })
@@ -663,12 +663,12 @@ describe('TalentLayer protocol global testing', function () {
 
       await talentLayerService
         .connect(deployer)
-        .updateAllowedTokenList(randomTokenAddress, true, minTokenWhitelistTranscationFees)
+        .updateAllowedTokenList(randomTokenAddress, true, minTokenWhitelistTransactionAmount)
       expect(await talentLayerService.isTokenAllowed(randomTokenAddress)).to.be.true
 
       await talentLayerService
         .connect(deployer)
-        .updateAllowedTokenList(randomTokenAddress, false, minTokenWhitelistTranscationFees)
+        .updateAllowedTokenList(randomTokenAddress, false, minTokenWhitelistTransactionAmount)
       expect(await talentLayerService.isTokenAllowed(randomTokenAddress)).to.be.false
     })
 
@@ -775,6 +775,31 @@ describe('TalentLayer protocol global testing', function () {
       )
     })
 
+    it('Bob can t create a proposal with an amount under the transcation limit amount ', async function () {
+      // Proposal on the Open service n 1
+      const rateToken = '0xC01FcDfDE3B2ABA1eab76731493C617FfAED2F10'
+      const platform = await talentLayerPlatformID.getPlatform(alicePlatformId)
+      const alicePlatformProposalPostingFee = platform.servicePostingFee
+
+      // Bob creates a proposal on Platform 1
+      await expect(
+        talentLayerService
+          .connect(bob)
+          .createProposal(
+            bobTlId,
+            1,
+            rateToken,
+            9,
+            alicePlatformId,
+            'proposal1FromBobToAlice1Service',
+            proposalExpirationDate,
+            {
+              value: alicePlatformProposalPostingFee,
+            },
+          ),
+      ).to.be.revertedWith('Amount is too low')
+    })
+
     it('Bob can create his first proposal for an Open service n°1 from Alice', async function () {
       // Proposal on the Open service n 1
       const bobTid = await talentLayerID.ids(bob.address)
@@ -793,7 +818,7 @@ describe('TalentLayer protocol global testing', function () {
           bobTlId,
           1,
           rateToken,
-          1,
+          15,
           alicePlatformId,
           'proposal1FromBobToAlice1Service',
           proposalExpirationDate,
@@ -812,7 +837,7 @@ describe('TalentLayer protocol global testing', function () {
       // Proposal data check after the proposal
 
       expect(proposalDataAfter.rateToken).to.be.equal(rateToken)
-      expect(proposalDataAfter.rateAmount.toString()).to.be.equal('1')
+      expect(proposalDataAfter.rateAmount.toString()).to.be.equal('15')
       expect(proposalDataAfter.dataUri).to.be.equal('proposal1FromBobToAlice1Service')
       expect(proposalDataAfter.ownerId).to.be.equal(bobTlId)
       expect(proposalDataAfter.status.toString()).to.be.equal('0')
@@ -830,7 +855,7 @@ describe('TalentLayer protocol global testing', function () {
           carolTlId,
           1,
           rateToken,
-          2,
+          16,
           bobPlatformId,
           'proposal1FromCarolToAlice1Service',
           proposalExpirationDate,
@@ -848,14 +873,14 @@ describe('TalentLayer protocol global testing', function () {
       const platform = await talentLayerPlatformID.getPlatform(alicePlatformId)
       const alicePlatformProposalPostingFee = platform.proposalPostingFee
 
-      expect(
+      await expect(
         talentLayerService
           .connect(carol)
           .createProposal(
             carolTlId,
             1,
             nonListedRateToken,
-            2,
+            16,
             alicePlatformId,
             'proposal1FromCarolToAlice1Service',
             proposalExpirationDate,
@@ -869,7 +894,7 @@ describe('TalentLayer protocol global testing', function () {
       const rateToken = '0xC01FcDfDE3B2ABA1eab76731493C617FfAED2F10'
 
       const proposalDataBefore = await talentLayerService.getProposal(1, bobTid)
-      expect(proposalDataBefore.rateAmount.toString()).to.be.equal('1')
+      expect(proposalDataBefore.rateAmount.toString()).to.be.equal('15')
 
       await talentLayerService
         .connect(bob)
@@ -877,13 +902,13 @@ describe('TalentLayer protocol global testing', function () {
           bobTlId,
           1,
           rateToken,
-          2,
+          18,
           'updateProposal1FromBobToAlice1Service',
           proposalExpirationDate,
         )
 
       const proposalDataAfter = await talentLayerService.getProposal(1, bobTid)
-      expect(proposalDataAfter.rateAmount.toString()).to.be.equal('2')
+      expect(proposalDataAfter.rateAmount.toString()).to.be.equal('18')
       expect(proposalDataAfter.dataUri).to.be.equal('updateProposal1FromBobToAlice1Service')
     })
 
@@ -1141,7 +1166,7 @@ describe('TalentLayer protocol global testing', function () {
             bobTlId,
             serviceId,
             rateToken,
-            1,
+            15,
             alicePlatformId,
             'proposalOnService',
             proposalExpirationDate,
