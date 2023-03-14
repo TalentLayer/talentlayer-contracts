a78f70eaa2cc5e7c693229d29b9a097d9a9c9002
Akumaw <dev@akumaw.com>
2023-03-14 12:46:12 +0100

    Fix duplicate key value violates unique constraint in sub in subgraph

--- scripts/playground/constants.ts
+++ scripts/playground/constants.ts
@@ -8,6 +8,7 @@ export const disputeId = 0
 export const arbitrationFeeTimeout = 3600 * 24 * 30
 export const rulingId = 1
 export const cid = 'QmQLVYemsvvqk58y8UTrCEp8MrcQaMzzT2e2duDEmFG99Z'
+export const cid2 = 'QmQLVYemsvvqk58y8UTrCEp8MrcQaMzzT2eOyhgT45UytG'
 export const metaEvidenceCid = 'QmQ2hcACF6r2Gf8PDxG4NcBdurzRUopwcaYQHNhSah6a8v'
 export const now = Math.floor(Date.now() / 1000)
 export const proposalExpirationDate = now + 60 * 60 * 24 * 15
