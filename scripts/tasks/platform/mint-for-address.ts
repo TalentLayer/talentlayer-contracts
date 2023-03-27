c639d8c97dd54b26dcb85ba24bc300dcbf4b718a
Romain <romain@talentlayer.org>
2023-03-27 12:13:10 +0100

    Update mint-for-address.ts

--- scripts/tasks/platform/mint-for-address.ts
+++ scripts/tasks/platform/mint-for-address.ts
@@ -5,7 +5,7 @@ import { DeploymentProperty, getDeploymentProperty } from '../../../.deployment/
  * @notice This task is used to mint a new TalentLayer ID for a user with a given address
  * @param {string} address - The wallet address of the user
  * @param {string} handle - The handle of the user
- * @dev Example of script use: "npx hardhat mint-for-address --address 0x5FbDB2315678afecb367f032d93F642f64180aa3 --network mumbai"
+ * @dev Example of script use: "npx hardhat mint-for-address --address 0x5FbDB2315678afecb367f032d93F642f64180aa3 --handle miguel --network mumbai"
  */
 task('mint-for-address', 'Mints a new TalentLayer ID for a given address')
   .addParam('address', "The user's address")
