a46261f904111c262cd1ac9ee4a3f1d945f81627
Romain <romain@talentlayer.org>
2023-03-28 12:03:20 +0100

    increase min arbitration fee timeout

--- contracts/TalentLayerPlatformID.sol
+++ contracts/TalentLayerPlatformID.sol
@@ -153,7 +153,7 @@ contract TalentLayerPlatformID is ERC721Upgradeable, AccessControlUpgradeable, U
         _setupRole(OWNER_ROLE, msg.sender);
         mintFee = 0;
         validArbitrators[address(0)] = true; // The zero address means no arbitrator.
-        updateMinArbitrationFeeTimeout(1 days); // TODO: update this value
+        updateMinArbitrationFeeTimeout(10 days);
         // Increment counter to start platform ids at index 1
         nextPlatformId.increment();
         mintStatus = MintStatus.ONLY_WHITELIST;
--- test/utils/constant.ts
+++ test/utils/constant.ts
@@ -4,10 +4,10 @@ export const cid2 = 'QmcbtH86xKGM4rNhpzcYMEjGF9qKMQ5Rdep8zfe3ndLtV1'
 export const metaEvidenceCid = 'QmQ2hcACF6r2Gf8PDxG4NcBdurzRUopwcaYQHNhSah6a8v'
 export const evidenceCid = 'QmNSARUuUMHkFcnSzrCAhmZkmQu7ViK18sPkg48xnbAmv4'
 export const now = Math.floor(Date.now() / 1000)
-export const proposalExpirationDate = now + 60 * 60 * 24 * 15
+export const proposalExpirationDate = now + 60 * 60 * 24 * 30 // note: as we increase time in several tests, better to be high
 export const expiredProposalDate = now - 60 * 60 * 24 * 30
 export const feeDivider = 10000
-export const arbitrationFeeTimeout = 3600 * 24
+export const arbitrationFeeTimeout = 3600 * 24 * 10
 export const ethAddress = '0x0000000000000000000000000000000000000000'
 
 export enum TransactionStatus {
