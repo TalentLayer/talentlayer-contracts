862a0b68824272036295f1a4ac43c79609b82121
Romain <romain@talentlayer.org>
2023-03-28 13:03:42 +0100

    Fix comments and var names

--- contracts/TalentLayerPlatformID.sol
+++ contracts/TalentLayerPlatformID.sol
@@ -446,6 +446,7 @@ contract TalentLayerPlatformID is ERC721Upgradeable, AccessControlUpgradeable, U
     /**
      * @notice Update Platform name mapping and emit event after mint.
      * @param _platformName Name of the platform.
+     * @param _platformAddress Address of the platform.
      * @dev Increments the nextTokenId counter.
      */
     function _afterMint(string memory _platformName, address _platformAddress) private returns (uint256) {
@@ -464,6 +465,10 @@ contract TalentLayerPlatformID is ERC721Upgradeable, AccessControlUpgradeable, U
         return platformId;
     }
 
+    /**
+     * @notice Validate characters used in the handle, only alphanumeric, only lowercase characters, - and _ are allowed but as first one
+     * @param handle Handle to validate
+     */
     function _validateHandle(string calldata handle) private pure {
         bytes memory byteHandle = bytes(handle);
         uint256 byteHandleLength = byteHandle.length;
@@ -542,7 +547,7 @@ contract TalentLayerPlatformID is ERC721Upgradeable, AccessControlUpgradeable, U
                                 platformName,
                                 '", "image":"',
                                 image,
-                                unicode'", "description": "Talent Layer Platform ID"}'
+                                unicode'", "description": "TalentLayer Platform ID"}'
                             )
                         )
                     )
@@ -592,91 +597,98 @@ contract TalentLayerPlatformID is ERC721Upgradeable, AccessControlUpgradeable, U
 
     /**
      * @notice Emit when new Platform ID is minted.
-     * @param _platformOwnerAddress Address of the owner of the PlatformID
-     * @param _platformId The Platform ID of the platform
-     * @param _platformName Name of the platform
-     * @param _fee Fee paid to mint the Platform ID
-     * @param _arbitrationFeeTimeout Timeout to pay arbitration fee
+     * @param platformOwnerAddress Address of the owner of the PlatformID
+     * @param platformId The Platform ID
+     * @param platformName Name of the platform
+     * @param fee Fee paid to mint the Platform ID
+     * @param arbitrationFeeTimeout Timeout to pay arbitration fee
      */
     event Mint(
-        address indexed _platformOwnerAddress,
-        uint256 _platformId,
-        string _platformName,
-        uint256 _fee,
-        uint256 _arbitrationFeeTimeout
+        address indexed platformOwnerAddress,
+        uint256 platformId,
+        string platformName,
+        uint256 fee,
+        uint256 arbitrationFeeTimeout
     );
 
     /**
      * @notice Emit when Cid is updated for a platform.
-     * @param _platformId The Platform ID of the platform
-     * @param _newCid New URI
+     * @param platformId The Platform ID
+     * @param newCid New URI
      */
-    event CidUpdated(uint256 indexed _platformId, string _newCid);
+    event CidUpdated(uint256 indexed platformId, string newCid);
 
     /**
      * @notice Emit when mint fee is updated
-     * @param _mintFee The new mint fee
+     * @param mintFee The new mint fee
      */
-    event MintFeeUpdated(uint256 _mintFee);
+    event MintFeeUpdated(uint256 mintFee);
 
     /**
      * @notice Emit when the fee is updated for a platform
-     * @param _originServiceFeeRate The new fee
+     * @param platformId The Platform Id
+     * @param originServiceFeeRate The new fee
      */
-    event OriginServiceFeeRateUpdated(uint256 _platformId, uint16 _originServiceFeeRate);
+    event OriginServiceFeeRateUpdated(uint256 platformId, uint16 originServiceFeeRate);
 
     /**
      * @notice Emit when the fee is updated for a platform
-     * @param _originValidatedProposalFeeRate The new fee
+     * @param platformId The Platform Id
+     * @param originValidatedProposalFeeRate The new fee
      */
-    event OriginValidatedProposalFeeRateUpdated(uint256 _platformId, uint16 _originValidatedProposalFeeRate);
+    event OriginValidatedProposalFeeRateUpdated(uint256 platformId, uint16 originValidatedProposalFeeRate);
 
     /**
      * @notice Emit after the arbitrator is updated for a platform
-     * @param _platformId The Platform Id
-     * @param _arbitrator The address of the new arbitrator
-     * @param _extraData The new extra data for the arbitrator
+     * @param platformId The Platform Id
+     * @param arbitrator The address of the new arbitrator
+     * @param extraData The new extra data for the arbitrator
      */
-    event ArbitratorUpdated(uint256 _platformId, Arbitrator _arbitrator, bytes _extraData);
+    event ArbitratorUpdated(uint256 platformId, Arbitrator arbitrator, bytes extraData);
 
     /**
      * @notice Emit after the arbitration fee timeout is updated for a platform
-     * @param _platformId The Platform Id
-     * @param _arbitrationFeeTimeout The new arbitration fee timeout
+     * @param platformId The Platform Id
+     * @param arbitrationFeeTimeout The new arbitration fee timeout
      */
-    event ArbitrationFeeTimeoutUpdated(uint256 _platformId, uint256 _arbitrationFeeTimeout);
+    event ArbitrationFeeTimeoutUpdated(uint256 platformId, uint256 arbitrationFeeTimeout);
 
     /**
      * @notice Emit after the minimum arbitration fee timeout is updated
-     * @param _minArbitrationFeeTimeout The new arbitration fee timeout
+     * @param minArbitrationFeeTimeout The new arbitration fee timeout
      */
-    event MinArbitrationFeeTimeoutUpdated(uint256 _minArbitrationFeeTimeout);
+    event MinArbitrationFeeTimeoutUpdated(uint256 minArbitrationFeeTimeout);
 
     /**
      * @notice Emit when the service posting fee is updated for a platform
-     * @param _servicePostingFee The new fee
+     * @param platformId The Platform Id
+     * @param servicePostingFee The new fee
      */
-    event ServicePostingFeeUpdated(uint256 _platformId, uint256 _servicePostingFee);
+    event ServicePostingFeeUpdated(uint256 platformId, uint256 servicePostingFee);
 
     /**
      * @notice Emit when the proposal posting fee is updated for a platform
-     * @param _proposalPostingFee The new fee
+     * @param platformId The Platform Id
+     * @param proposalPostingFee The new fee
      */
-    event ProposalPostingFeeUpdated(uint256 _platformId, uint256 _proposalPostingFee);
+    event ProposalPostingFeeUpdated(uint256 platformId, uint256 proposalPostingFee);
 
     /**
      * @notice Emit when the signer address is updated for a platform
-     * @param _signer The new signer address
+     * @param platformId The Platform Id
+     * @param signer The new signer address
      */
-    event SignerUpdated(uint256 _platformId, address _signer);
+    event SignerUpdated(uint256 platformId, address signer);
 
     /**
      * @notice Emit when the minting status is updated
+     * @param mintStatus The new mint status
      */
-    event MintStatusUpdated(MintStatus _mintStatus);
+    event MintStatusUpdated(MintStatus mintStatus);
 
     /**
      * @notice Emit when a platform is whitelisted
+     * @param user The new address whitelited
      */
-    event UserWhitelisted(address indexed _user);
+    event UserWhitelisted(address indexed user);
 }
